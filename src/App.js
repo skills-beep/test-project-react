0f71ffdd10cc1fc6b5469838535cee1a47c78896
Bishal <bishalsharma@gmail.com>
2024-12-04 12:23:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Greet from './components/Greet.js'
+import Welcome from './components/Welcome.js';
+
+import './App.css';
+
+function App() {
+   
+ return(
+  <> 
+  <Greet/>
+  <Welcome/>
+  </>
+ )
+ 
+}
+
+
+export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,6 @@
+import React from "react";
+// function Greet(){
+//     return <h1>Hello Bishal</h1>
+// }
+const Greet = () =><h1>Hello Bishal</h1>
+export default Greet;
